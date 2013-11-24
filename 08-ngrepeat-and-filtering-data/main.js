c92e24ab3050638672a1071af4229423a6a66807
Brian Yingling <brian_yingling@yahoo.com>
2013-11-24 12:57:27 -0500

    watched lessons 8-9

---
+++ 08-ngrepeat-and-filtering-data/main.js
@@ -0,0 +1,34 @@
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Avengers', function() {
+  var Avengers = {};
+
+  Avengers.cast = [
+    {
+      name: 'Clark Gregg',
+      character: 'Agent Phil Coulson'
+    },
+    {
+      name: 'Samuel L. Jackson',
+      character: 'Nick Fury'
+    },
+    {
+      name: 'Gwyneth Paltrow',
+      character: 'Pepper Potts'
+    },
+    {
+      name: 'Robert Downey, Jr.',
+      character: 'Tony Stark / Iron Man'
+    },
+    {
+      name: 'Chris Evans',
+      character: 'Steve Rogers / Captain America'
+    }
+  ];
+
+  return Avengers;
+});
+
+function AvengersCtrl($scope, Avengers) {
+  $scope.avengers = Avengers;
+}
