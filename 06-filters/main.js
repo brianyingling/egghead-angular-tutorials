958955d962d3fe1433fe77c48b47aba30abaf6ca
Brian Yingling <brian_yingling@yahoo.com>
2013-11-24 11:47:58 -0500

    finished lessons 5-7

--- 06-filters/main.js
+++ 06-filters/main.js
@@ -5,6 +5,12 @@ myApp.factory('Data', function() {
   return {message: "I'm data from a message"};
 });
 
+myApp.filter('reverse', function(Data) {
+  return function(text) {
+    return text.split("").reverse().join('') + Data.message;
+  };
+});
+
 function FirstCtrl($scope, Data) {
   $scope.data = Data;
 }
